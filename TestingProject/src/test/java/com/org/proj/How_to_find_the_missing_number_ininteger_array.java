33104d4c840f1ade01fc941b81365c1f61422345
rajubhaskar7258 <47634973+rajubhaskar7258@users.noreply.github.com>
2019-04-01 14:12:08 +0530

    upxdate array

---
+++ TestingProject/src/test/java/com/org/proj/How_to_find_the_missing_number_ininteger_array.java
@@ -0,0 +1,30 @@
+package com.org.proj;
+
+import java.util.Arrays;
+
+public class How_to_find_the_missing_number_ininteger_array
+{
+    
+	public static void main(String[] args) {
+		int[] a={1,8,6,4,3,5,2,9};
+		Arrays.sort(a);
+		
+		int consucutive=1;
+		
+		for (int i = 1; i < a.length; i++) {
+			if (a[i-1]+consucutive==a[i]) {
+				
+			}
+			
+			
+		}
+		
+		
+		
+		
+	}
+
+	
+	
+	
+}
